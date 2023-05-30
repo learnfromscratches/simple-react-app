5dbcf5a44061745e73498edd7aa844123d0f2400
learnfromscratches <unnikrishnankj@outlook.com>
2023-05-30 08:09:15 +0530

    small UI changes

--- src/components/card.js
+++ src/components/card.js
@@ -8,8 +8,8 @@ const Card = props =>{
         <div className={styles.card} >
             <img src={props.thumbnailImage} alt='Failed to load image'/>
             <h3>{props.title}</h3>
-            <span>MRP</span>
-            <span>Offer details</span>
+            <span>{props.actualPrice}</span>
+            <span>{props.price}</span>
         </div>
     )
 }
